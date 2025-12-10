b1ffe66c8d6ddb7f7421be85b3e415de68f11c06
Matheus Nathan <mateusnazareth@outlook.com>
2025-12-10 16:57:38 +0000

    vercel

--- backend2/db.js
+++ backend2/db.js
@@ -1,4 +1,10 @@
 const mysql = require('mysql2/promise');
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
